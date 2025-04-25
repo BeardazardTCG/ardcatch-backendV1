59df5c13af41ece12231fa9a371b57e8d2485a5b
BeardazardTCG <david.laird88@gmail.com>
2025-04-25 13:13:28 +0100

    Update server.js

--- server.js
+++ server.js
@@ -8,6 +8,11 @@ app.get('/', (req, res) => {
   res.send('CardCatch backend is live!');
 });
 
+// Test endpoint for card price fetch
+app.get('/api/fetchCardPrice', (req, res) => {
+  res.json({ message: 'fetchCardPrice endpoint is working' });
+});
+
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
 });
